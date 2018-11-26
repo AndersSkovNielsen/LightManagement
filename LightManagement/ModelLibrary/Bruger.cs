1d981688294d5175b6c405e1f0e4efe8cfa46a25
Joachim Grubbe Frank <jgf.easj@gmail.com>
2018-11-26 11:10:51 +0100

    Update Bruger.cs

--- LightManagement/ModelLibrary/Bruger.cs
+++ LightManagement/ModelLibrary/Bruger.cs
@@ -6,5 +6,32 @@ namespace ModelLibrary
 {
     class Bruger
     {
+        private int _id;
+        private string _brugernavn;
+        private string _password;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string BrugerNavn
+        {
+            get { return _brugernavn; }
+            set { _brugernavn = value; }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        public Bruger()
+        { }
+        public Bruger(int id, string brugerNavn, string password)
+        {
+            _id = id;
+            _brugernavn = brugerNavn;
+            _password = password;
+        }
     }
 }
