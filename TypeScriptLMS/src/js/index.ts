ad4f03774b512898f4391b4896b146fb5ec1c9c0
Joachim Grubbe Frank <jgf.easj@gmail.com>
2018-11-28 10:29:30 +0100

    Kan hente fra azure

--- TypeScriptLMS/src/js/index.ts
+++ TypeScriptLMS/src/js/index.ts
@@ -1,4 +1,9 @@
-import axios, { AxiosPromise } from 'axios';
+//import axios, { AxiosPromise } from 'axios';
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index";
+
 
 interface Person {
     firstName: string;
@@ -28,3 +33,70 @@ for (let i: number = 0; i < elements.length; i++) {
         }
     });
 }
+
+
+let uri: string = "https://ande-easj-rest.azurewebsites.net/api/bruger";
+
+
+let elementById: HTMLDivElement = <HTMLDivElement>document.getElementById("content2");
+let elementById2: HTMLDivElement = <HTMLDivElement>document.getElementById("content3");
+let InputConverter: HTMLInputElement = <HTMLInputElement> document.getElementById("InputConverter");
+
+axios.get(uri)
+    .then(function (response: AxiosResponse): void {
+        elementById.innerHTML = JSON.stringify(response.data);
+    })
+    .catch(function (error: AxiosError): void {
+        elementById.innerHTML = error.message;
+    });
+
+    axios.get(uri + InputConverter)
+    .then(function (response: AxiosResponse): void {
+        elementById2.innerHTML = JSON.stringify(response.data);
+    })
+    .catch(function (error: AxiosError): void {
+        elementById2.innerHTML = error.message;
+    });
+
+
+function AxionGetBid(): void
+{
+
+    axios.get('https://ande-easj-rest.azurewebsites.net/api/bruger')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+}
+
+function AxionGetBidById(id:number): void
+{
+
+    axios.get('https://ande-easj-rest.azurewebsites.net/api/bruger' + id)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+}
+
+function CTO(): void
+{ 
+    let InputConverter: HTMLInputElement = <HTMLInputElement> document.getElementById("InputConverter");
+  let id: number = Number(InputConverter.value); 
+  axios.get(uri + id)
+  .then(function (response: AxiosResponse): void {
+      elementById2.innerHTML = JSON.stringify(response.data);
+  })
+  .catch(function (error: AxiosError): void {
+      elementById2.innerHTML = error.message;
+  });
+
+}
+let toOuncesButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("GetIdButton");
+toOuncesButton.addEventListener("click", CTO);
