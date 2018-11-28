86c151ebadeeb593cb944abc02485c56b5f53b95
Joachim Grubbe Frank <jgf.easj@gmail.com>
2018-11-28 09:07:11 +0100

    Update Proxy.cs

--- LightManagement/UDPProxy/Proxy.cs
+++ LightManagement/UDPProxy/Proxy.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace UDPProxy
 {
     class Proxy
     {
+        private readonly int PORT;
+
+        //public UDPReceiver(int port)
+        //{
+        //    PORT = port;
+        //}
+
+        public void start()
+        {
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+            using (UdpClient receiverSock = new UdpClient(10100)) // Raspberry port nummer her
+            {
+                while (true)
+                {
+                    HandleOneRequest(receiverSock, remoteEP);
+                }
+            }
+        }
+        private static void HandleOneRequest(UdpClient receiverSock, IPEndPoint remoteEP)
+        {
+            byte[] data = receiverSock.Receive(ref remoteEP);
+            String inStr = Encoding.ASCII.GetString(data);
+
+            Console.WriteLine("modtaget " + inStr);
+            Console.WriteLine("sender ip=" + remoteEP.Address + " port=" + remoteEP.Port);
+
+            byte[] outData = Encoding.ASCII.GetBytes(inStr.ToUpper());
+            receiverSock.Send(outData, outData.Length, remoteEP);
+        }
     }
 }
