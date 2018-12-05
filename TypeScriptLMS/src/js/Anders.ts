0bfb3b0f3811010d2e10a2ae95a17eabd04150e2
AndersSkovNielsen <anderssnielsen@live.dk>
2018-12-05 12:59:29 +0100

    Error Prevention, in progress

--- TypeScriptLMS/src/js/Anders.ts
+++ TypeScriptLMS/src/js/Anders.ts
@@ -21,39 +21,67 @@ fjernButton.addEventListener("click", fjernFunction)
 //Button void functions
 function tilføjFunction(): void{
 
-let tilføjID: HTMLInputElement = <HTMLInputElement> document.getElementById("TilføjID");
-let tilføjName: HTMLInputElement = <HTMLInputElement> document.getElementById("TilføjName");
-let tilføjKode: HTMLInputElement = <HTMLInputElement> document.getElementById("TilføjKode");
+  let errorMessage: HTMLDivElement = <HTMLDivElement> document.getElementById("ErrorMessage");
 
-let id: number = Number(tilføjID.value);
-let navn: string = String(tilføjName.value);
-let kode: string = String(tilføjKode.value);
 
-console.log(id)
-console.log(navn);
-console.log(kode);
+  let tilføjID: HTMLInputElement = <HTMLInputElement> document.getElementById("TilføjID");
+  let tilføjName: HTMLInputElement = <HTMLInputElement> document.getElementById("TilføjName");
+  let tilføjKode: HTMLInputElement = <HTMLInputElement> document.getElementById("TilføjKode");
 
-axios.post(uri, 
+  let id: number = Number(tilføjID.value);
+  let navn: string = String(tilføjName.value);
+  let kode: string = String(tilføjKode.value);
+
+  console.log(id)
+  console.log(navn);
+  console.log(kode);
+
+  //if (AllowPost(id) == true)
+  //{
+    axios.post(uri, 
+      {
+          Id: id,
+          Brugernavn: navn,  
+          Kodeord: kode
+      })
+      .then(function(response) 
+      {
+          console.log(response.status);
+      });
+  }
+  //else
+  //{
+  //  errorMessage.innerHTML = "ID allerede i brug. Vælg et andet.";
+  //}
+
+//}
+
+function fjernFunction(): void
 {
-    Id: id,
-    Brugernavn: navn,  
-    Kodeord: kode
-})
-.then(function(response) 
-{
-    console.log(response.status);
-});
+
+  let sletBruger: HTMLInputElement = <HTMLInputElement> document.getElementById("SletBruger");
+  let id: number = Number(sletBruger.value);
+
+  axios.delete(uri + id)
+  .then(function(response){
+  console.log(response.data); // ex.: { user: 'Your User'}
+  console.log(response.status); // ex.: 200
+  })
 }
 
-    function fjernFunction(): void
+function AllowPost(id: number): any
+{
+  axios.get(uri + id)
+  .then(function(response)
+  {
+    console.log(response.data);
+    console.log(response.status);
+
+    if (response.status == 200)
     {
-
-    let sletBruger: HTMLInputElement = <HTMLInputElement> document.getElementById("SletBruger");
-    let id: number = Number(sletBruger.value);
-
-    axios.delete(uri + id)
-  .then(function(response){
-    console.log(response.data); // ex.: { user: 'Your User'}
-    console.log(response.status); // ex.: 200
-    })
+      return false;
+    }
+    else
+    return true
+  })
 }
