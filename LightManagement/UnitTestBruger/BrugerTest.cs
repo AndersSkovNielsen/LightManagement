2481175240ba4404217bb2a6c4b43c26f857ebdc
AndersSkovNielsen <anderssnielsen@live.dk>
2018-12-05 10:33:28 +0100

    Unit Test komplet

--- LightManagement/UnitTestBruger/BrugerTest.cs
+++ LightManagement/UnitTestBruger/BrugerTest.cs
@@ -1,8 +1,12 @@
 using System.Collections.Generic;
+using System.Net.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ModelLibrary;
 using LightREST;
 using LightREST.DBUtil;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System;
 
 namespace UnitTestBruger
 {
@@ -12,22 +16,33 @@ namespace UnitTestBruger
         [TestMethod]
         public void TestListeLængde()
         {
+            string uri = "http://ande-easj-rest.azurewebsites.net/api/sensor/";
             List<Bruger> liste = new List<Bruger>();
             ManageBruger manager = new ManageBruger();
             int firstCount = 0;
             int secondCount = 0;
+            Bruger b = new Bruger(1, "ansn", "kodeord");
 
-            liste = manager.HentAlleBruger();
+            using (HttpClient client = new HttpClient())
+            {
+                liste = JsonConvert.DeserializeObject<List<Bruger>>(client.GetStringAsync(uri).Result);
 
-            firstCount = liste.Count;
+                firstCount = liste.Count;
 
-            manager.FjernBruger(1);
+                client.DeleteAsync(uri + 1);
 
-            liste = manager.HentAlleBruger();
+                liste = JsonConvert.DeserializeObject<List<Bruger>>(client.GetStringAsync(uri).Result);
 
-            secondCount = liste.Count;
+                secondCount = liste.Count;
 
-            Assert.AreNotEqual(firstCount, secondCount);
+                Assert.AreNotEqual(firstCount, secondCount);
+
+                String json = JsonConvert.SerializeObject(b);
+                StringContent content = new StringContent(json);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                client.PostAsync(uri, content);
+            }
         }
     }
 }
