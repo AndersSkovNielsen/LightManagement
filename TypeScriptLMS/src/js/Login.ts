58ab19782506d0d9c6b9d430ba0aa7692464a5ff
Joachim Grubbe Frank <jgf.easj@gmail.com>
2018-12-06 13:02:04 +0100

    push

--- TypeScriptLMS/src/js/Login.ts
+++ TypeScriptLMS/src/js/Login.ts
@@ -5,10 +5,10 @@ AxiosError
 } from "../../node_modules/axios/index";
 let uri: string = "https://ande-easj-rest.azurewebsites.net/api/bruger/";
 let elementById: HTMLDivElement = <HTMLDivElement>document.getElementById("content1");
-let LoginNavn: HTMLDivElement = <HTMLDivElement>document.getElementById("LoginNavn");
-let Kodeord: HTMLDivElement = <HTMLDivElement>document.getElementById("LoginKodeord");
-let ID: HTMLDivElement = <HTMLDivElement>document.getElementById("theId");
-let Bruger: string = ID + " - " + LoginNavn + " - " + Kodeord + "<br/>";
+let LoginNavn: HTMLInputElement = <HTMLInputElement>document.getElementById("LoginNavn");
+let Kodeord: HTMLInputElement = <HTMLInputElement>document.getElementById("LoginKodeord");
+let ID: HTMLInputElement = <HTMLInputElement>document.getElementById("theId");
+let Bruger: string = "";
 let Login: string = "";
 
 interface IPost {
@@ -20,6 +20,7 @@ interface IPost {
 }
 
 let result: IPost[] = new Array(30); //hvordan erklæres array?? //YATA!
+
 function AxiosData (response:AxiosResponse):void
 {
     let i:number=0;
@@ -29,14 +30,9 @@ function AxiosData (response:AxiosResponse):void
             result[i].brugernavn=JSON.stringify(e.brugernavn);
             result[i].id=e.id;
             result[i].kodeord=e.kodeord;
-            result[i].sensor=e.sensor;
+            //result[i].sensor=e.sensor;
             i=i+1;
 
-result[i].brugernavn=e.brugernavn;
-result[i].id=e.id;
-result[i].kodeord=e.kodeord;
-result[i].sensor=e.sensor;
-i=i+1;
 
 
         
@@ -60,59 +56,77 @@ i=i+1;
 
  function HovedmenuKnap(): void
  {
-  console.log("Hovedmenu side er kaldt")
-  window.location.href = "index.htm";
- }
+    sammenlign();
+    console.log(Sammenlign)
+     if(Sammenlign == true)
+    {
+        console.log("Hovedmenu side er kaldt")
+        window.location.href = "index.htm";
+    }
+}
+        
 
  
 
-    let BrugerListe =[ 
-        axios.get(uri).then(function (response: AxiosResponse): void
-    {
-        let i: number;
-
-            for(i=0; i<10; i++)
-            {
-                Login = Login + response.data[i].id + " - " + response.data[i].brugernavn + " - " + response.data[i].kodeord + "<br/>";
-            }
-        
-        elementById.innerHTML = Login;
-    })]
+   
 
 
-    function ÆndreBruger(): string 
-    {
-        axios.get(uri).then(function (response: AxiosResponse): void
-        {
-            Bruger = Bruger + response.data[1].id + " - " + LoginNavn + " - " + Kodeord + "<br/>";
-            
-        })
-        return Bruger;
-    }
+    
     //Skaber en boolean so skal checkes for true eller false for adgang til næste side.
     let Sammenlign: boolean = false;
     //En function som sammenligner den nuværende bruger, som bliver ændret til det der skrives ind på siden.
     function sammenlign(): void
     {
-        
-        BrugerListe.forEach(element => 
-            {
-            if(Bruger == Login)
-            {
-                Sammenlign = true;         
-                stop();
-            }
-            });
+        console.log("sammenlign begyndt")
+        result.forEach(element => 
+        {
+            console.log("loop begyndt")
+            //if(String(LoginNavn) == element.brugernavn)
+            //{
+            //    if(String(Kodeord) == element.kodeord)
+            //    Sammenlign = true;         
+            //    stop();
+           // }
+           if(Login == Bruger)
+           {
+               console.log(Login + Bruger)
+               Sammenlign = true;
+           }
+        });
     }
     //skaber button element der føre tilbage til index siden
     let MenuButton:HTMLButtonElement=<HTMLButtonElement> document.getElementById("Index");
     //If checker om menuButton er null. Fikser Fejlen hvor den loader for hurtigt.
     if (MenuButton)
     {
-        sammenlign();
-        if(Sammenlign == true)
-        {
-            MenuButton.addEventListener("click", HovedmenuKnap);
-        }   
+        MenuButton.addEventListener("click", HovedmenuKnap);
         
     }
+
+
+
+
+
+//ikke god kode
+
+   // function ÆndreBruger(): string 
+   // {
+   //     axios.get(uri).then(function (response: AxiosResponse): void
+   //     {
+   //         Bruger = Bruger + response.data[1].id + " - " + LoginNavn + " - " + Kodeord + "<br/>";
+   //         
+   //     })
+   //     return Bruger;
+   // }
+   // let BrugerListe =[ 
+   //     axios.get(uri).then(function (response: AxiosResponse): void
+   // {
+   //     let i: number;
+//
+  //          for(i=0; i<10; i++)
+   //         {
+   //             Login = response.data[i].id + " - " + response.data[i].brugernavn + " - " + response.data[i].kodeord + "<br/>";
+   //         }
+   //     
+   //     elementById.innerHTML = Login;
+   // })]
